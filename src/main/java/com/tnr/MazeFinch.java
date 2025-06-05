ee8dcce9f51d505bfe0f725103aca05a5566aaf1
remiflopper <jpeebles0001@mymail.lausd.net>
2025-06-05 11:15:19 -0700

    Update MazeFinch.java

--- src/main/java/com/tnr/MazeFinch.java
+++ src/main/java/com/tnr/MazeFinch.java
@@ -7,13 +7,24 @@ public class MazeFinch {
         Finch myFinch = new Finch();
 
         // What do I want my finch to do?
-        myFinch.setMove("F", 100, 50);
-        myFinch.setTurn("L", 90, 100);
-        myFinch.setMove("F", 45, 50);
-        myFinch.setTurn("L", 90, 100);
-        myFinch.setMove("F", 38, 50);
-        myFinch.setTurn("R", 90, 100);
-        myFinch.setMove("F", 30, 50);
+        myFinch.setMove("F", 105, 60);
+        myFinch.setTurn("L", 90, 60);
+        myFinch.setMove("F", 40, 50);
+        myFinch.setTurn("L", 90, 60);
+        myFinch.setMove("F", 35, 40);
+        myFinch.setTurn("R", 90, 60);
+        myFinch.setMove("F", 45, 60);
+        myFinch.setTurn("R", 90, 50);
+        myFinch.setMove("F", 110, 60);
+        myFinch.setTurn("R", 80, 60);
+        myFinch.setMove("F", 8, 60);
+        myFinch.setTurn("R", 80, 60);
+        myFinch.setMove("F", 2, 60);
+        myFinch.setTurn("L", 90, 60);
+        myFinch.setMove("F", 50, 40);
+        myFinch.setTurn("L", 45, 40);
+        myFinch.setMove("F", 26, 40);
+
 
 
 
